f639d12458d71a043bfe240f62654588c39af0d4
Iman-Waris <hussnainwaris438@gmail.com>
2024-07-18 12:05:26 +0500

    Add project files.

---
+++ oop/Program.cs
@@ -0,0 +1,232 @@
+﻿//Task 1
+//using System;
+//namespace app
+//{
+
+//public class Person
+//    {
+//         public string FirstName = "iman";
+//       public  string LastName = "waris";
+
+
+//    }
+
+// public class Employee:Person
+//    {
+
+//      public  int EmployeeId = 123;
+//        public void totaldata()
+//        {
+//            Console.Write( FirstName+ " " +LastName +" "+ EmployeeId);
+
+//        }
+
+
+//    }
+
+
+
+
+
+//class Car
+//    {
+
+//static void Main()
+//        {
+//            Employee obj = new Employee();
+//            obj.totaldata();
+
+//        }
+
+
+//    }
+
+
+
+//}
+
+//Task 2
+
+//using System;
+//namespace app
+//{
+
+//    public class BaseController
+//    {
+
+//        public string GetWelcomeMessage()
+//        {
+
+//            return " Welcome!";
+
+//        }
+
+//    }
+//    public class HomeController : BaseController
+//    {
+//        public void message()
+//        {
+//            string text = GetWelcomeMessage();
+//            Console.WriteLine(text);
+
+//        }
+//    }
+
+//    class Car
+//    {
+//        static void Main()
+//        {
+//            HomeController obj = new HomeController();
+//            obj.message();
+
+//        }
+//    }
+//}
+
+//Task 3
+
+//using System;
+//namespace app
+//{
+
+// public class BaseEntity
+//    {
+
+
+//      public  int id=12345678;
+
+//    }
+
+//public class Product : BaseEntity
+//    {
+
+//      public   string name = "iman";
+//     public    double price = 56.7894;
+
+//public void copy()
+//        {
+//            Console.WriteLine(name+"  "+ price+"  "+id);
+
+//        }
+
+//    }
+//class Copy
+//    {
+//static void Main()
+//        {
+//            Product obj = new Product();
+//            obj.copy();
+//        }
+
+//    }
+
+
+//}
+
+
+
+//task 4
+//using System;
+//namespace app
+//{
+//public class BaseViewModel
+//    {
+
+//      public   int id = 12345;
+//      public   string name = "iman";
+
+//    }
+
+//    public class ProductViewModel: BaseViewModel
+//    {
+
+//        public double price = 345.2345;
+
+//        public void mix()
+
+//        {
+
+//            Console.WriteLine(id + "   " + name + "  " + price);
+
+//        }
+
+//    }
+//   public  class Copy
+//    {
+
+//static void Main()
+//        {
+
+//            ProductViewModel obj = new ProductViewModel();
+//            obj.mix();
+
+
+//        }
+
+//    }
+//}
+
+
+//question 5
+
+//using System;
+//namespace app
+//{
+
+//    public class BaseService
+//    {
+//public void GetEntityName()
+//        {
+
+//            string name = "BaseEntity";
+
+//        }
+
+
+//    }
+
+//    public class ProductService: BaseService
+//    {
+
+
+
+
+
+//    }
+//using System;
+//namespace app
+//    {
+//    class Car
+//    {
+//        static void Main()
+//        {
+
+
+//Console.WriteLine(" \bHELLO WORLD\b");
+
+//        }
+
+//    }
+//}
+
+
+
+
+
+
+//public class Copy
+//    {
+//static void Main()
+//        {
+
+
+
+
+//        }
+
+
+//    }
+
+//}
+
+//print("hello world");
